033fdf86daf1320c887620770fde08811941a1c4
Rebecca_Colavin <colavin@gmail.com>
2015-09-21 18:28:49 -0700

    cleaned up
    Got rid of a bunch of #identifiers and replaced with variable names.
    corrected indentation for readability

--- scope.js
+++ scope.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
 
-var rowcounter = 0;
+  var rowcounter = 0;
 
-function newFancyCounter(){
-var counter=0;
-return{
-  more: function(){
-    counter += 1;
-    return counter;
-    },
-  less: function(){
-    counter -= 1;
-    return counter;
-    },
-    reset: function(){
-      counter = 0;
-      return counter;
-    },
+  function newFancyCounter(){
+    var counter=0;
+    return{
+      more: function(){
+        counter += 1;
+        return counter;
+      },
+      less: function(){
+        counter -= 1;
+        return counter;
+      },
+      reset: function(){
+          counter = 0;
+          return counter;
+      },
+    }
   }
-}
 
 
 
 function createRow(){
   rowcounter++;
   var localcounter=rowcounter;
-  var $mydiv=$("<div/>").attr({id:"mydiv"+localcounter, class:"col-md-12 main"});
   var cnt = newFancyCounter();
+
+  var $top=$("#newrow");
+  var $mydiv=$("<div/>").attr({id:"mydiv"+localcounter, class:"col-md-12 main"});
   var $span = $('<span/>').attr({class:'className', id:"counter"+localcounter}).html("0   ");
   var $add = $('<input/>').attr({ type: 'button', class: 'guess', id: 'add'+localcounter, value: "+"});
   var $delete = $('<input/>').attr({ type: 'button', class: 'guess', id: 'delete'+localcounter, value: "delete"});
@@ -34,68 +36,47 @@ function createRow(){
   var $reset = $('<input/>').attr({ type: 'button', class: 'guess', id: 'reset'+localcounter, value: "reset"});
 
 
-    $("#newrow").append($mydiv);
-    $('#mydiv' + localcounter).append($span);
-    $("#mydiv"+localcounter).append($add);
-    $('#mydiv' + localcounter).append($delete);
-    $('#mydiv' + localcounter).append($reset);
-    $('#mydiv' + localcounter).append($less);
+  $top.append($mydiv);
+  $mydiv.append($span);
+  $mydiv.append($add);
+  $mydiv.append($delete);
+  $mydiv.append($reset);
+  $mydiv.append($less);
 
 
 
-    $add.click(function(event){
-      temp=cnt.more();
-      $("span#counter"+localcounter).html(temp+" ");
-      });
+  $add.click(function(event){
+    temp=cnt.more();
+    $span.html(temp+" ");
+  });
 
-      $less.click(function(event){
-        temp=cnt.less();
-        $("span#counter"+localcounter).html(temp+" ");
-        });
+  $less.click(function(event){
+    temp=cnt.less();
+    $span.html(temp+" ");
+  });
 
-        $reset.click(function(event){
-          temp=cnt.reset();
-          $("span#counter"+localcounter).html(temp+" ");
-          });
+  $reset.click(function(event){
+    temp=cnt.reset();
+    $span.html(temp+" ");
+  });
 
-      $delete.click(function(event){
-      alert("you will delete a row");
-      // $('#add'+rowcounter).remove();
-      // $('#counter'+rowcounter).remove();
-      $("#mydiv" + localcounter).remove();
+  $delete.click(function(event){
+    alert("you will delete a row");
+    $mydiv.remove();
+  });
 
-    });
-
-    };
+};
 
 
 
 
-var addrow=$("#addrow");
+  var addrow=$("#addrow");
 
 
-addrow.click(function(event){
+  addrow.click(function(event){
     createRow();
   });
 
 
-// var reset_btn= $("#reset_btn");
-// var decrease= $("#dec_btn");
-// var increase= $("#inc_btn");
-// var cnt = newFancyCounter();
-// var count= $("#counter");
-//
-//
-// reset_btn.click(function(event){
-//   count.html(cnt.reset);
-// });
-//
-// increase.click(function(event){
-//   count.html(cnt.more);
-// });
-//
-// decrease.click(function(event){
-//   count.html(cnt.less);
-// });
 
 });
